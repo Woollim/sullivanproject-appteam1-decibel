2414b920e99f8e4d3348ad208f4901efa97d0d63
riflockle7 <riflockle7@gmail.com>
2020-07-18 20:12:01 +0900

    2020.07.18 (Sat)
    - 병찬님과 이야기나눈 스토리 방향대로 코드 수정

--- lib/HistoryPage.dart
+++ lib/HistoryPage.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutterapp/SqliteUtil.dart';
+import 'SoundData.dart';
 
 /// 소음 측정 내역을 보여주는 화면
 /// 첫 페이지에서 우측 상단 버튼을 누르면 볼 수 있는 화면입니다.
 class HistoryPage extends StatefulWidget {
+  HistoryPage({this.title}) : super();
+
+  var title;
+
   @override
   State<StatefulWidget> createState() => HistoryPageState();
 }
 
 class HistoryPageState extends State<StatefulWidget> {
+  List<SoundData> history = List();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,5 +37,13 @@ class HistoryPageState extends State<StatefulWidget> {
     print(str);
 
     return str;
+  }
+  /// DB로부터 데이터를 받아옵니다.
+  void getAll() {
+    SqliteUtil.getSoundData().then((value) {
+      setState(() {
+        history = value;
+      });
+    });
   }
 }
