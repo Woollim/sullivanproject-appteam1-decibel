a7b5e232aa2180a6b4398fa1283e1f6fe791c4ac
riflockle7 <riflockle7@gmail.com>
2020-07-18 20:13:12 +0900

    2020.07.18 (Sat)
    - 병찬님과 이야기나눈 스토리 방향대로 코드 수정

--- lib/main.dart
+++ lib/main.dart
@@ -11,16 +11,18 @@ void main() {
 /// 처음에 실행시켜줄 화면을 보여줍니다.
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  var title = '설리번 프로젝트';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '설리번 프로젝트',
+      title: title,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
 //      home: HomePage(),
-      home: HistoryPage(),
+      home: HistoryPage(title: "층간소음 녹음내역"),
     );
   }
 }
