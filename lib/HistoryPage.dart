328db50978006c88aa8c5633ca7855a674045ef7
riflockle7 <riflockle7@gmail.com>
2020-07-19 23:56:54 +0900

    2020.07.19 (Sun)
    - 새로운 스토리 라인 적용에 따른 코드 수정 (2차)

--- lib/HistoryPage.dart
+++ lib/HistoryPage.dart
@@ -14,13 +14,58 @@ class HistoryPage extends StatefulWidget {
 }
 
 class HistoryPageState extends State<HistoryPage> {
-  List<SoundData> history = List();
+  List<SoundData> history = [];
+
+  @override
+  void initState() {
+    super.initState();
+    // addTestData();
+    getAll();
+  }
 
   @override
   Widget build(BuildContext context) {
+    getAll();
     return Scaffold(
       appBar: AppBar(title: Text("층간소음 녹음내역")),
-      body: Center(child: Text("여러분 모두 만나서 반가워요")),
+      body: Column(
+        children: <Widget>[
+          generateRow(context, -1),
+          Expanded(
+              child: ListView.builder(
+                itemCount: history.length,
+                scrollDirection: Axis.vertical,
+                itemBuilder: (_, index) => generateRow(context, index),
+              ),
+          )
+        ],
+      ),
+    );
+  }
+
+  /// 한 줄에 보여줄 데이터를 만든다. */
+  Widget generateRow(BuildContext context, int index) {
+    if (index == -1)
+      return Row(children: <Widget>[
+        generateBorderText(context, 0.5, '시간 '),
+        generateBorderText(context, 0.25, '평균 데시벨'),
+        generateBorderText(context, 0.25, '피크 데시벨')
+      ]);
+    else
+      return Row(children: <Widget>[
+        generateBorderText(context, 0.5, history[index].date),
+        generateBorderText(context, 0.25, "${history[index].averageDecibel}"),
+        generateBorderText(context, 0.25, "${history[index].maxDecibel}")
+      ]);
+  }
+
+  /// 스타일링된 텍스트뷰를 반환한다. */
+  Widget generateBorderText(BuildContext context, double ratio, String text) {
+    return Container(
+      width: MediaQuery.of(context).size.width * ratio,
+      padding: const EdgeInsets.all(5.0),
+      decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
+      child: Text(text, textAlign: TextAlign.center),
     );
   }
 
@@ -31,5 +76,10 @@ class HistoryPageState extends State<HistoryPage> {
         history = value;
       });
     });
+  }
+
+  /// DB에 더미 데이터를 추가합니다.
+  void addTestData(){
+    SqliteUtil.onAdd(SoundData(date: parseDate(new DateTime.now()), averageDecibel: 2, maxDecibel: 3));
   }
 }
--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'HistoryPage.dart';
 
 /// 첫 페이지
 /// 소음을 측정하는 화면입니다.
@@ -11,7 +12,19 @@ class HomePageState extends State<StatefulWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("소음 측정기")),
+      appBar: AppBar(title: Text("소음 측정기"), actions: [
+        IconButton(
+          icon: new Icon(Icons.search),
+          highlightColor: Colors.white,
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (_) => HistoryPage(),
+              ),
+            );
+          },
+        ),
+      ]),
       body: Center(child: Text("여러분 모두 만나서 반가워요")),
     );
   }
